f8a90f528038889676379a6dadc807f6b71613ec
osmansufy <osmansufy20@gmail.com>
2023-12-27 11:08:27 +0600

    [Make] PHP routes

---
+++ router.php
@@ -0,0 +1,27 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+define('BASE_URI', '/php-basic');
+$routes = [
+    BASE_URI . '/' => 'controllers/index.php',
+    BASE_URI . '/about' => 'controllers/about.php',
+    BASE_URI . '/contact' => 'controllers/contact.php',
+
+];
+
+function routeToController($uri, $routes) {
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+function abort($code = 404) {
+    http_response_code($code);
+    require "views/{$code}.php";
+
+    die();
+}
+
+routeToController($uri, $routes);
